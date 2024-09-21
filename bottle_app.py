031e771bb2cd235bf18d697cbe98e197bfc9d2e8
sf <sreedfenton@gmail.com>
2024-09-20 19:20:16 -0700

    added the beginnings of a session manager from my other python project. i am thinking of making the current index a dict with session id : index to make it multi user without a db yet. more to come tmrw

--- bottle_app.py
+++ bottle_app.py
@@ -1,6 +1,7 @@
 from quiz import generate_vocab_choices_list, generate_vocab_choices_dict, generate_combined_vocabulary_dict, get_valid_user_selection, generate_combined_vocabulary_dict, question_generator, print_missed_words, quiz
 from bottle import route, post, request, run, template, redirect
 
+index = {} #until session management
 
 @route('/')
 def index():
